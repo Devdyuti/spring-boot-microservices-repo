355dcac5df0926c542b66246b4b5b9af3f51f219
Devdyuti <singhking5414@hotmail.com>
2020-04-04 17:34:42 +0530

    update limit service-2

---
+++ spring-boot-microservice-parent/spring-cloud-config-server/src/main/java/org/microservice/poc/App.java
@@ -0,0 +1,13 @@
+package org.microservice.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
